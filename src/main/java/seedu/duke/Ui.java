6996123fa7997b49a6027301583856872b494dd8
irvinseet <irvinseet@gmail.com>
2021-10-10 23:12:56 +0800

    add help message in Ui class

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -16,4 +16,18 @@ public class Ui {
         System.out.println("Sorry, we are only able to process name, date and process for now");
         System.out.println("The following tags are available: n/ d/ a/");
     }
+
+    public static void help() {
+        System.out.println("Available tags: n/name d/date a/amount");
+        System.out.println("Order of tags does not matter.");
+        System.out.println("List of commands available. " +
+                "Square brackets \"[ ]\" identifies an optional argument.");
+        System.out.println("\u2022 view");
+        System.out.println("\u2022 add n/NAME a/amount [d/YYYY-MM-DD]");
+        System.out.println(INDENT + "Example: add n/chicken rice a/3.50 d/2021-09-30 ");
+        System.out.println("\u2022 delete n/NAME a/amount d/YYYY-MM-DD");
+        System.out.println(INDENT + "Example: delete n/chicken rice a/3.50 d/2021-09-30");
+        System.out.println("\u2022 exit");
+    }
+
 }
