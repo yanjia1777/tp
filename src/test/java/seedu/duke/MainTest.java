06e4aea397f71f485e349fbe39e83236b3429de1
Yitching <seetohyitching@gmail.com>
2021-10-10 19:16:16 +0800

    Update test for AddFunction and move tests into a separate file for abstraction

--- src/test/java/seedu/duke/MainTest.java
+++ src/test/java/seedu/duke/MainTest.java
@@ -1,28 +1,18 @@
 package seedu.duke;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 class MainTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
-    }
-
-    @Test
-    public void addExpense_oneAddition_success() {
-        ExpenseList expense = new ExpenseList();
-        LocalDate date = LocalDate.now();
-        expense.addExpense("burger", date.toString(), "10");
-    }
-
-    @Test
-    public void addExpense_twoAdditions_success() {
-        ExpenseList expense = new ExpenseList();
-        LocalDate date = LocalDate.now();
-        expense.addExpense("movie", date.toString(), "20");
     }
 }
