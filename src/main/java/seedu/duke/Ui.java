176dde690646bda4e392e2e8e1dffeb21ce40c13
irvinseet <irvinseet@gmail.com>
2021-10-12 00:05:20 +0800

    add print statements for categories

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -2,6 +2,14 @@ package seedu.duke;
 
 public class Ui {
     protected static final String INDENT = "    ";
+    public static final int CAT_NUM_FOOD = 1;
+    public static final int CAT_NUM_ENTERTAINMENT = 2;
+    public static final int CAT_NUM_TRANSPORTATION = 3;
+    public static final int CAT_NUM_HOUSEHOLD = 4;
+    public static final int CAT_NUM_APPAREL = 5;
+    public static final int CAT_NUM_BEAUTY = 6;
+    public static final int CAT_NUM_GIFT = 7;
+    public static final int CAT_NUM_OTHERS = 0;
 
     public static void startup() {
         System.out.println("Hello! I'm Mint");
@@ -30,4 +38,41 @@ public class Ui {
         System.out.println("\u2022 exit");
     }
 
+    public static String printIndividualCategory(int catNum) {
+        switch (catNum) {
+        case CAT_NUM_FOOD:
+            return("      Food      ");
+        case CAT_NUM_ENTERTAINMENT:
+            return(" Entertainment  ");
+        case CAT_NUM_TRANSPORTATION:
+            return(" Transportation ");
+        case CAT_NUM_HOUSEHOLD:
+            return("   Household    ");
+        case CAT_NUM_APPAREL:
+            return("    Apparel     ");
+        case CAT_NUM_BEAUTY:
+            return("     Beauty     ");
+        case CAT_NUM_GIFT:
+            return("      Gift      ");
+        case CAT_NUM_OTHERS:
+            return("     Others     ");
+        default:
+            return("No category found");
+        }
+    }
+
+    public static void printCategories() {
+        System.out.println(" Cat no.  Category");
+        System.out.println(Ui.INDENT + "c/1.  Food");
+        System.out.println(Ui.INDENT + "c/2.  Entertainment");
+        System.out.println(Ui.INDENT + "c/3.  Transportation");
+        System.out.println(Ui.INDENT + "c/4.  Household");
+        System.out.println(Ui.INDENT + "c/5.  Apparel");
+        System.out.println(Ui.INDENT + "c/6.  Beauty");
+        System.out.println(Ui.INDENT + "c/7.  Gift");
+        System.out.println("default c/0  Others");
+    }
 }
+
+
+
