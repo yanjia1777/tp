90cc55011521c164758159c800e55093562b61d2
pos0414 <gimins1202@gmail.com>
2021-10-20 11:53:33 +0900

    Delete unused methods

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -291,13 +291,6 @@ public class Parser {
         return 0;
     }
 
-
-    private void checkMissingNameFieldOfUserInput(String userInput) throws MintException {
-        if (!userInput.contains("n/")) {
-            throw new MintException("Please include name!");
-        }
-    }
-
     private ArrayList<String> identifyValidTags(String userInput, String[] mandatoryTags) throws MintException {
         ArrayList<String> validTags = new ArrayList<>();
         ArrayList<String> invalidTags = new ArrayList<>();
@@ -350,27 +343,6 @@ public class Parser {
         } catch (MintException e) {
             throw new MintException(e.getMessage());
         }
-    }
-
-    private void checkMissingFieldOfUserInput(String userInput) throws MintException {
-        String[] keyDelimiters = command.equals("add")
-                ? new String[]{"n/", "a/"}
-                : new String[]{"n/", "d/", "a/", "c/"};
-        ArrayList<String> missingDelimiters = identifyDelimiters(userInput, keyDelimiters);
-        if (missingDelimiters.size() > 0) {
-            constructErrorMessage(missingDelimiters);
-            throw new MintException(constructErrorMessage(missingDelimiters).toString());
-        }
-    }
-
-    private ArrayList<String> identifyDelimiters(String userInput, String[] keyDelimiters) {
-        ArrayList<String> missingDelimiters = new ArrayList<>();
-        for (String delimiter : keyDelimiters) {
-            if (!userInput.contains(delimiter)) {
-                missingDelimiters.add(delimiter);
-            }
-        }
-        return missingDelimiters;
     }
 
     private StringBuilder constructErrorMessage(ArrayList<String> missingDelimiters) throws MintException {
