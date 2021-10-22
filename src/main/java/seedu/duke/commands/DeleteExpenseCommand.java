856551cca273af7c69808d5289564a80384bf36d
Yitching <seetohyitching@gmail.com>
2021-10-23 02:05:46 +0800

    Fix checkstyle errors for comments indentation

--- src/main/java/seedu/duke/commands/DeleteExpenseCommand.java
+++ src/main/java/seedu/duke/commands/DeleteExpenseCommand.java
@@ -32,7 +32,7 @@ public class DeleteExpenseCommand {
     //    }
 
     public void deleteExpense(Expense expense, ArrayList<Expense> expenseList) throws MintException {
-            //        Expense expense = new Expense(name, date, amount, catNum);
+        //        Expense expense = new Expense(name, date, amount, catNum);
         if (expenseList.contains(expense)) {
             //            logger.log(Level.INFO, "User deleted expense: " + expense);
             System.out.println("I have deleted: " + expense);
@@ -41,7 +41,7 @@ public class DeleteExpenseCommand {
         //            if (isCurrentMonthExpense(expense)) {
         //                CategoryList.deleteSpending(expense);
         //            }
-        //            ExpenseListDataManager.deleteLineInTextFile(stringToDelete);
+    //            ExpenseListDataManager.deleteLineInTextFile(stringToDelete);
         }
     }
 }
