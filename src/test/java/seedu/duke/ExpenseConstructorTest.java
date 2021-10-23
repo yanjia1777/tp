013f9cd8214e26c72f058aac7c30ccb7ddbdc513
irvinseet <irvinseet@gmail.com>
2021-10-23 19:45:23 +0800

    update Junit test

--- src/test/java/seedu/duke/ExpenseConstructorTest.java
+++ src/test/java/seedu/duke/ExpenseConstructorTest.java
@@ -15,11 +15,10 @@ public class ExpenseConstructorTest {
     @Test
     void createExpense_allFieldsValid_success() {
         String name = "Samurai Burger";
-        String date = "2021-10-12";
-        String amount = "7.50";
-        String catNum = "0";
-        CategoryList.initialiseCategories();
-        Expense expense = new Expense(name, date, amount, catNum);
-        assertEquals("Food | 2021-10-12 | Samurai Burger | $7.50", expense.toString());
+        LocalDate date = LocalDate.parse("2021-10-12");
+        Double amount = 7.50;
+        ExpenseCategory category = ExpenseCategory.FOOD;
+        Expense expense = new Expense(name, date, amount, category);
+        assertEquals("FOOD | 2021-10-12 | Samurai Burger | $7.50", expense.toString());
     }
 }
