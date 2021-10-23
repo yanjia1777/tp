99696b7ef7efa0d20ce6d16e3668db85d0c21ee1
irvinseet <irvinseet@gmail.com>
2021-10-23 19:45:55 +0800

    Restructure Entry and Expense classes

--- src/main/java/seedu/duke/Entry.java
+++ src/main/java/seedu/duke/Entry.java
@@ -4,42 +4,17 @@ import java.time.LocalDate; // import the LocalDate class
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
-public class Entry {
-    public static DateTimeFormatter dateFormatter
-            = DateTimeFormatter.ofPattern("[yyyy-MM-dd][yyyy-M-dd][yyyy-MM-d][yyyy-M-d]"
-            + "[dd-MM-yyyy][d-MM-yyyy][d-M-yyyy][dd-M-yyyy]"
-            + "[dd MMM yyyy][d MMM yyyy][dd MMM yy][d MMM yy]");
-    public static final int CAT_NUM_OTHERS = 7;
-    private int catNum;
-    private String name;
-    private LocalDate date;
-    private double amount;
+public abstract class Entry {
+    protected String name;
+    protected LocalDate date;
+    protected double amount;
 
-    public Entry() {
-        catNum = CAT_NUM_OTHERS; //others
-        date = LocalDate.of(2021, 1, 1);
-        amount = 0;
-    }
-
-    public Entry(String name, String date, String amount, String catNum) {
-        this.catNum = Integer.parseInt(catNum);
-        this.name = name;
-        this.date = LocalDate.parse(date, dateFormatter);
-        this.amount = Double.parseDouble(amount);
-    }
-
-    public Entry(String name, String date, String amount) {
-        this.catNum = CAT_NUM_OTHERS;
-        this.name = name;
-        this.date = LocalDate.parse(date, dateFormatter);
-        this.amount = Double.parseDouble(amount);
-    }
 
     public String getName() {
         return name;
     }
 
-    public void setDescription(String name) {
+    public void setName(String name) {
         this.name = name;
     }
 
@@ -51,63 +26,18 @@ public class Entry {
         this.date = date;
     }
 
-    public int getCatNum() {
-        return this.catNum;
-    }
-
-    public void setCatNum(int catNum) {
-        this.catNum = catNum;
-    }
-
     public double getAmount() {
         return amount;
-    }
-
-    public String getAmountString() {
-        return Double.toString(amount);
-    }
-
-    public String getCat() {
-        return CategoryList.getCatName(this.catNum);
-    }
-
-    public String getCatIndent() {
-        return CategoryList.getCatNameIndented(this.catNum);
     }
 
     public void setAmount(double amount) {
         this.amount = amount;
     }
 
-    public String toString() {
-        return getCat() + " | " + getDate() + " | "
-                + getName() + " | $" + String.format("%,.2f", getAmount());
-    }
+    public abstract Enum getCategory();
 
-    public String viewToString() {
-        return getCatIndent() + "| " + getDate() + " | "
-                + getName() + " | $" + String.format("%,.2f", getAmount());
-    }
+    public abstract String toString();
 
-    //@@author nipafx-reusedS
-    //Reused from https://www.sitepoint.com/implement-javas-equals-method-correctly/
-    //with minor modifications
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null) {
-            return false;
-        }
-        if (getClass() != object.getClass()) {
-            return false;
-        }
+    public abstract boolean equals(Object object);
 
-        Expense expense = (Expense) object;
-        boolean isNameEqual = Objects.equals(name, expense.getName());
-        boolean isDateEqual = Objects.equals(date, expense.getDate());
-        boolean isAmountEqual = Objects.equals(amount, expense.getAmount());
-        boolean isCategoryEqual = Objects.equals(catNum, expense.getCatNum());
-        return isNameEqual && isDateEqual && isAmountEqual && isCategoryEqual;
-    }
 }
--- src/main/java/seedu/duke/Expense.java
+++ src/main/java/seedu/duke/Expense.java
@@ -4,19 +4,18 @@ import java.time.LocalDate; // import the LocalDate class
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
-public class Expense extends Entry {
-    public static DateTimeFormatter dateFormatter
-            = DateTimeFormatter.ofPattern("[yyyy-MM-dd][yyyy-M-dd][yyyy-MM-d][yyyy-M-d]"
-            + "[dd-MM-yyyy][d-MM-yyyy][d-M-yyyy][dd-M-yyyy]"
-            + "[dd MMM yyyy][d MMM yyyy][dd MMM yy][d MMM yy]");
-    public static final int CAT_NUM_OTHERS = 7;
 
-    private String name;
-    private LocalDate date;
-    private ExpenseCategory category;
-    private double amount;
-    
-    //ideally
+
+public class Expense extends Entry {
+    protected ExpenseCategory category;
+
+    public Expense(String name, double amount, ExpenseCategory category) {
+        this.name = name;
+        this.amount = amount;
+        this.date = LocalDate.now();
+        this.category = category;
+    }
+
     public Expense(String name, LocalDate date, Double amount, ExpenseCategory category) {
         this.name = name;
         this.date = date;
@@ -24,83 +23,25 @@ public class Expense extends Entry {
         this.category = category;
     }
 
-    public Expense(String name, String date, String amount, String catNum) {
-        this.catNum = Integer.parseInt(catNum);
-        this.name = name;
-        this.date = LocalDate.parse(date, dateFormatter);
-        this.amount = Double.parseDouble(amount);
-    }
-
-    public Expense(String name, String date, String amount, ExpenseCategory category) {
-        this.name = name;
-        this.date = LocalDate.parse(date, dateFormatter);
-        this.amount = Double.parseDouble(amount);
+    public void setCategory(ExpenseCategory category) {
         this.category = category;
     }
 
-    public Expense(String name, String date, String amount) {
-        this.catNum = CAT_NUM_OTHERS;
-        this.name = name;
-        this.date = LocalDate.parse(date, dateFormatter);
-        this.amount = Double.parseDouble(amount);
-    }
-
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setDescription(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public int getCatNum() {
-        return this.catNum;
-    }
-
-    public void setCatNum(int catNum) {
-        this.catNum = catNum;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-
-    public ExpenseCategory getCat() {
+    @Override
+    public ExpenseCategory getCategory() {
         return category;
     }
 
-    public String getCatIndent() {
-        return CategoryList.getCatNameIndented(this.catNum);
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
+    @Override
     public String toString() {
-        return getCat() + " | " + getDate() + " | "
-                + getName() + " | $" + String.format("%,.2f", getAmount());
-    }
-
-    public String viewToString() {
-        return getCatIndent() + "| " + getDate() + " | "
+        return getCategory() + " | " + getDate() + " | "
                 + getName() + " | $" + String.format("%,.2f", getAmount());
     }
 
     //@@author nipafx-reusedS
     //Reused from https://www.sitepoint.com/implement-javas-equals-method-correctly/
     //with minor modifications
+    @Override
     public boolean equals(Object object) {
         if (this == object) {
             return true;
@@ -116,7 +57,7 @@ public class Expense extends Entry {
         boolean isNameEqual = Objects.equals(name, expense.name);
         boolean isDateEqual = Objects.equals(date, expense.date);
         boolean isAmountEqual = Objects.equals(amount, expense.amount);
-        boolean isCategoryEqual = Objects.equals(catNum, expense.catNum);
+        boolean isCategoryEqual = Objects.equals(category, expense.category);
         return isNameEqual && isDateEqual && isAmountEqual && isCategoryEqual;
     }
 }
