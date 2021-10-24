9fd522add93b1eee95f7f3c1b563512ab1b03c37
yanjia1777 <yanjia1777@gmail.com>
2021-10-24 17:58:00 +0800

    Fix checkstyle errors

--- src/main/java/seedu/duke/Expense.java
+++ src/main/java/seedu/duke/Expense.java
@@ -8,17 +8,17 @@ import java.util.Objects;
 public class Expense extends Entry {
     protected ExpenseCategory category;
 
-    public Expense (String name, LocalDate date, double amount, ExpenseCategory category) {
+    public Expense(String name, LocalDate date, double amount, ExpenseCategory category) {
         super(name, date, amount);
         this.category = category;
         this.type = Type.Expense;
     }
 
-    public ExpenseCategory getCategory () {
+    public ExpenseCategory getCategory() {
         return category;
     }
 
-    public String getCategoryIndented () {
+    public String getCategoryIndented() {
         double length = getCategory().toString().length();
         int leftIndent = (int) Math.floor((16 - length) / 2);
         int rightIndent = (int) Math.ceil((16 - length) / 2);
@@ -31,18 +31,18 @@ public class Expense extends Entry {
         return Ui.getIndent(leftIndent, rightIndent, getCategory().toString()).toString();
     }
 
-    public void setCategory (ExpenseCategory category) {
+    public void setCategory(ExpenseCategory category) {
         this.category = category;
     }
 
-    public String toString () {
+    public String toString() {
         return getType() + " | " + getCategoryIndented() + " | " + getDate() + " | "
                 + getNameIndented() + " |-$" + String.format("%,.2f", getAmount());
 
     }
 
     @Override
-    public boolean equals (Object object) {
+    public boolean equals(Object object) {
         return false;
     }
 }
