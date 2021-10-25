0689fe198822cc985fa92e1746d5a781f41207b8
irvinseet <irvinseet@gmail.com>
2021-10-25 13:10:43 +0800

    implemented Budget and BudgetManager

---
+++ src/main/java/seedu/duke/budget/Budget.java
@@ -0,0 +1,45 @@
+package seedu.duke.budget;
+
+import seedu.duke.entries.Entry;
+import seedu.duke.entries.Expense;
+import seedu.duke.entries.ExpenseCategory;
+import seedu.duke.entries.Type;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
+public abstract class Budget {
+    protected ExpenseCategory category;
+    protected String name;
+    protected double limit;
+
+    public ExpenseCategory getCategory() {
+        return this.category;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setLimit(double amount) {
+        this.limit = amount;
+    }
+
+    public double getLimit() {
+        return this.limit;
+    }
+
+    public double getMontlhySpending(ArrayList<Entry> entries) {
+        double amount = 0;
+        for (Entry entry : entries) {
+            if (entry.getType() == Type.Expense
+                    && (entry.getCategory() == this.category)
+                    && (entry.getDate().getMonth() == LocalDate.now().getMonth())
+                    && entry.getDate().getYear() == LocalDate.now().getYear()) {
+                amount += entry.getAmount();
+            }
+        }
+        return amount;
+    }
+}
---
+++ src/main/java/seedu/duke/commands/SetBudgetCommand.java
@@ -0,0 +1,30 @@
+package seedu.duke.commands;
+
+
+import seedu.duke.budget.BudgetManager;
+import seedu.duke.entries.Entry;
+import seedu.duke.entries.ExpenseCategory;
+import seedu.duke.exception.MintException;
+import seedu.duke.finances.NormalFinanceManager;
+import seedu.duke.finances.RecurringFinanceManager;
+import seedu.duke.utility.Ui;
+
+
+public class SetBudgetCommand extends Command {
+    private final ExpenseCategory category;
+    private final double amount;
+
+    public SetBudgetCommand(ExpenseCategory category, double amount) {
+        this.category = category;
+        this.amount = amount;
+    }
+
+    @Override
+    public void execute(NormalFinanceManager normalFinanceManager,
+                        RecurringFinanceManager recurringFinanceManager, BudgetManager budgetManager, Ui ui) {
+        budgetManager.setBudget(category, amount);
+        ui.printSetBudget(category,amount);
+    }
+}
+
+
