5479fcded92b0476adf80e4ec3b5cae509c07457
yanjia1777 <yanjia1777@gmail.com>
2021-11-03 14:28:23 +0800

    Fixed checkstyle errors

--- src/test/java/seedu/duke/ViewFunctionTest.java
+++ src/test/java/seedu/duke/ViewFunctionTest.java
@@ -73,28 +73,26 @@ class ViewFunctionTest {
             System.setOut(new PrintStream(output));
             view.view(outputArray,recurringOutputArray, ui);
             String expectedOutput = "Here is the list of your entries:\n" +
-                    "  Type  |  Category  |    Date    |      Name      |   Amount    | Every |   Until\n" +
-                    "Income  | COMMISSION | 2021-10-09 |    OnlyFans    | $300.00     | MONTH | 2027-04-01\n" +
-                    "Income  | COMMISSION | 2021-09-09 |    OnlyFans    | $300.00     | MONTH | 2027-04-01\n" +
-                    "Income  | COMMISSION | 2021-08-09 |    OnlyFans    | $300.00     | MONTH | 2027-04-01\n" +
-                    "Income  | COMMISSION | 2021-07-09 |    OnlyFans    | $300.00     | MONTH | 2027-04-01\n" +
-                    "Income  | COMMISSION | 2021-06-09 |    OnlyFans    | $300.00     | MONTH | 2027-04-01\n" +
-                    "Expense |   OTHERS   | 2021-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n" +
-                    "Expense |    FOOD    | 2021-02-01 | Samurai Burger |-$7.50       |       |\n" +
-                    "Expense |   OTHERS   | 2020-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n" +
-                    "Expense |   OTHERS   | 2019-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n" +
-                    "Expense |   OTHERS   | 2018-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n" +
-                    "Expense |   OTHERS   | 2017-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n" +
-                    "Expense |   OTHERS   | 2016-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n" +
-                    "Income  |    GIFT    | 2015-12-15 |    Lottery     | $250,000.00 |       |\n" +
-                    "Expense |   OTHERS   | 2015-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n" +
-                    "Expense |   OTHERS   | 2014-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n" +
-                    "Expense |   OTHERS   | 2013-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n" +
-                    "Expense |   OTHERS   | 2012-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n" +
-                    "                                        Net Total: | $251,346.50\n" +
-                    "Here is the list of recurring entries added to the above list:\n" +
-                    "Income  | COMMISSION | 2021-06-09 |    OnlyFans    | $300.00     | MONTH | 2027-04-01\n" +
-                    "Expense |   OTHERS   | 2012-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n";
+                    "  Type  |  Category  |    Date    |      Name      |   Amount    | Every |   Until\n"
+                    + "Income  | COMMISSION | 2021-10-09 |    OnlyFans    | $300.00     | MONTH | 2027-04-01\n"
+                    + "Income  | COMMISSION | 2021-09-09 |    OnlyFans    | $300.00     | MONTH | 2027-04-01\n"
+                    + "Income  | COMMISSION | 2021-08-09 |    OnlyFans    | $300.00     | MONTH | 2027-04-01\n"
+                    + "Income  | COMMISSION | 2021-07-09 |    OnlyFans    | $300.00     | MONTH | 2027-04-01\n"
+                    + "Income  | COMMISSION | 2021-06-09 |    OnlyFans    | $300.00     | MONTH | 2027-04-01\n"
+                    + "Expense |   OTHERS   | 2021-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n"
+                    + "Expense |    FOOD    | 2021-02-01 | Samurai Burger |-$7.50       |       |\n" + "Expense |   OTHERS   | 2020-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n"
+                    + "Expense |   OTHERS   | 2019-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n"
+                    + "Expense |   OTHERS   | 2018-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n"
+                    + "Expense |   OTHERS   | 2017-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n"
+                    + "Expense |   OTHERS   | 2016-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n"
+                    + "Income  |    GIFT    | 2015-12-15 |    Lottery     | $250,000.00 |       |\n" + "Expense |   OTHERS   | 2015-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n"
+                    + "Expense |   OTHERS   | 2014-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n"
+                    + "Expense |   OTHERS   | 2013-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n"
+                    + "Expense |   OTHERS   | 2012-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n"
+                    + "                                        Net Total: | $251,346.50\n"
+                    + "Here is the list of recurring entries added to the above list:\n"
+                    + "Income  | COMMISSION | 2021-06-09 |    OnlyFans    | $300.00     | MONTH | 2027-04-01\n"
+                    + "Expense |   OTHERS   | 2012-06-06 |   Maid Cafe    |-$14.60      | YEAR  | 2023-01-06\n";
             assertEquals(expectedOutput, output.toString());
         } catch (MintException e) {
             e.printStackTrace();
