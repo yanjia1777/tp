ad08818c974c45b7d42b4cba0122b8cc9e9f46db
pos0414 <gimins1202@gmail.com>
2021-11-04 22:50:09 +0900

    Update view command test

--- src/test/java/seedu/duke/ViewFunctionTest.java
+++ src/test/java/seedu/duke/ViewFunctionTest.java
@@ -60,7 +60,8 @@ class ViewFunctionTest {
                     + "Income  |   GIFT   | 2015-12-15 |    Lottery     | $250,000.00 |       |"
                     + System.lineSeparator()
                     + "                                      Net Total: | $249,992.50" + System.lineSeparator()
-                    + "Here is the list of recurring entries added to the above list:" + System.lineSeparator();
+                    + "Here is the list of all recurring entries, where some were added to the above list:"
+                    + System.lineSeparator();
             assertEquals(expectedOutput, output.toString());
         } catch (MintException e) {
             e.printStackTrace();
