6ccdf48626f34cb1b5ebaf20876b4eb258b51c1e
pos0414 <gimins1202@gmail.com>
2021-11-08 01:45:03 +0900

    Extract out common components for NormalFinanceManagerTest

--- src/test/java/seedu/duke/NormalFinanceManagerTest.java
+++ src/test/java/seedu/duke/NormalFinanceManagerTest.java
@@ -50,10 +50,11 @@ class NormalFinanceManagerTest {
         assertFalse(entryList.contains(query));
     }
 
+    LocalDate date1 = LocalDate.parse("2021-10-29", dateFormatter);
+    LocalDate date2 = LocalDate.parse("2021-11-29", dateFormatter);
+
     @Test
     public void filterEntryByKeywords_queryFullName_oneMatchingResult() {
-        LocalDate date1 = LocalDate.parse("2021-10-29", dateFormatter);
-        LocalDate date2 = LocalDate.parse("2021-11-29", dateFormatter);
         Expense targetEntry = new Expense("Pear", date1, 3.9, ExpenseCategory.FOOD);
         Expense dummyEntry = new Expense("Apple", date2, 5.0, ExpenseCategory.FOOD);
         entryList.add(targetEntry);
@@ -76,8 +77,6 @@ class NormalFinanceManagerTest {
 
     @Test
     public void filterEntryByKeywords_queryFullName_multipleMatchingResult() {
-        LocalDate date1 = LocalDate.parse("2021-10-29", dateFormatter);
-        LocalDate date2 = LocalDate.parse("2021-11-29", dateFormatter);
         Expense targetEntry1 = new Expense("pork", date1, 3.9, ExpenseCategory.FOOD);
         Expense targetEntry2 = new Expense("PoRk", date2, 5.0, ExpenseCategory.FOOD);
         entryList.add(targetEntry1);
@@ -100,8 +99,6 @@ class NormalFinanceManagerTest {
 
     @Test
     public void filterEntryByKeywords_queryFullName_noMatchingResult() {
-        LocalDate date1 = LocalDate.parse("2021-10-29", dateFormatter);
-        LocalDate date2 = LocalDate.parse("2021-11-29", dateFormatter);
         Expense targetEntry1 = new Expense("jelly", date1, 3.9, ExpenseCategory.FOOD);
         Expense targetEntry2 = new Expense("worm", date2, 5.0, ExpenseCategory.FOOD);
         entryList.add(targetEntry1);
@@ -123,8 +120,6 @@ class NormalFinanceManagerTest {
 
     @Test
     public void filterEntryByKeywords_queryNameWithAbbreviation_multipleMatchingResult() {
-        LocalDate date1 = LocalDate.parse("2021-10-29", dateFormatter);
-        LocalDate date2 = LocalDate.parse("2021-11-29", dateFormatter);
         LocalDate date3 = LocalDate.parse("2021-11-12", dateFormatter);
         Expense targetEntry1 = new Expense("Pear", date1, 1.0, ExpenseCategory.FOOD);
         Expense dummyEntry = new Expense("apple", date2, 200.2, ExpenseCategory.FOOD);
@@ -151,8 +146,6 @@ class NormalFinanceManagerTest {
 
     @Test
     public void filterEntryByKeywords_queryAmount_multipleMatchingResult() {
-        LocalDate date1 = LocalDate.parse("2021-10-29", dateFormatter);
-        LocalDate date2 = LocalDate.parse("2021-11-29", dateFormatter);
         Expense targetEntry1 = new Expense("Ice", date1, 3.9, ExpenseCategory.FOOD);
         Expense targetEntry2 = new Expense("yoyo", date2, 3.9, ExpenseCategory.OTHERS);
         entryList.add(targetEntry1);
@@ -175,8 +168,6 @@ class NormalFinanceManagerTest {
 
     @Test
     public void filterEntryByKeywords_queryDate_oneMatchingResult() {
-        LocalDate date1 = LocalDate.parse("2021-10-29", dateFormatter);
-        LocalDate date2 = LocalDate.parse("2021-11-29", dateFormatter);
         Income targetEntry = new Income("Salary", date1, 1000.0, IncomeCategory.SALARY);
         Expense dummyEntry = new Expense("banana", date2, 5.0, ExpenseCategory.FOOD);
         entryList.add(targetEntry);
@@ -198,8 +189,6 @@ class NormalFinanceManagerTest {
 
     @Test
     public void filterEntryByKeywords_queryCategory_oneMatchingResult() {
-        LocalDate date1 = LocalDate.parse("2021-10-29", dateFormatter);
-        LocalDate date2 = LocalDate.parse("2021-11-29", dateFormatter);
         Expense targetEntry = new Expense("choco", date1, 3.9, ExpenseCategory.FOOD);
         Expense dummyEntry = new Expense("movie", date2, 5.0, ExpenseCategory.ENTERTAINMENT);
         entryList.add(targetEntry);
@@ -222,8 +211,6 @@ class NormalFinanceManagerTest {
 
     @Test
     public void filterEntryByKeywords_queryNameAndDate_oneMatchingResult() {
-        LocalDate date1 = LocalDate.parse("2021-10-29", dateFormatter);
-        LocalDate date2 = LocalDate.parse("2021-11-29", dateFormatter);
         Expense targetEntry = new Expense("bread", date1, 3.9, ExpenseCategory.FOOD);
         Expense dummyEntry1 = new Expense("kinder", date2, 5.0, ExpenseCategory.FOOD);
         Income dummyEntry2 = new Income("Bread", date2, 5.0, IncomeCategory.OTHERS);
@@ -250,8 +237,6 @@ class NormalFinanceManagerTest {
 
     @Test
     public void filterEntryByKeywords_queryDateAndAmountAndCategory_oneMatchingResult() {
-        LocalDate date1 = LocalDate.parse("2021-10-29", dateFormatter);
-        LocalDate date2 = LocalDate.parse("2021-11-29", dateFormatter);
         Expense targetEntry = new Expense("bread", date1, 1.0, ExpenseCategory.FOOD);
         Expense dummyEntry1 = new Expense("kinder", date2, 5.0, ExpenseCategory.FOOD);
         Income dummyEntry2 = new Income("Bread", date1, 1.0, IncomeCategory.OTHERS);
@@ -276,85 +261,4 @@ class NormalFinanceManagerTest {
         entryList.remove(dummyEntry1);
         entryList.remove(dummyEntry2);
     }
-
-    /*
-    public void deleteEntry_existingItem_success() throws MintException {
-        NormalFinanceManager financeManager = new NormalFinanceManager();
-        String name = "Cheese";
-        LocalDate date = LocalDate.parse("2021-12-23", dateFormatter);
-        Double amount = 15.5;
-        ExpenseCategory category = ExpenseCategory.FOOD;
-
-        financeManager.entryList.add(new Expense("Cheese", date, amount, category));
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
-        financeManager.deleteEntry(new Expense(name, date, amount, category));
-        String expectedOutput  = "I have deleted: Food | 2021-12-23 | Cheese burger | $15.50"
-                + System.lineSeparator();
-        assertEquals(expectedOutput, outContent.toString());
-    }
-
-
-
-    @Test
-    public void deleteExpense_wrongAmountFormat_printErrorMessage() throws MintException {
-        ExpenseList expenseList = new ExpenseList();
-        RecurringExpenseList recurringExpenseList = new RecurringExpenseList();
-        Parser parser = new Parser();
-
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
-        parser.executeCommand("delete n/Cheese d/2021-12-23 a/15.5abc c/1", expenseList, recurringExpenseList);
-        String expectedOutput  = Parser.STRING_INCLUDE + "1. " + Parser.STRING_AMOUNT + System.lineSeparator();
-        assertEquals(expectedOutput, outContent.toString());
-    }
-
-    @Test
-    public void deleteExpense_wrongDateFormat_printErrorMessage() throws MintException {
-        ExpenseList expenseList = new ExpenseList();
-        RecurringExpenseList recurringExpenseList = new RecurringExpenseList();
-        Parser parser = new Parser();
-
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
-        parser.executeCommand("delete n/Cheese d/2021-12 a/15.5 c/1", expenseList, recurringExpenseList);
-        String expectedOutput  = Parser.STRING_INCLUDE + "1. " + Parser.STRING_DATE + System.lineSeparator();
-        assertEquals(expectedOutput, outContent.toString());
-    }
-
-    @Test
-    public void viewExpense_twoExpenses_success() throws MintException {
-        CategoryList.initialiseCategories();
-        ExpenseList expenseList = new ExpenseList();
-        expenseList.expenseList.add(new Expense("Cheese burger", "2021-12-23", "15.5"));
-        expenseList.expenseList.add(new Expense("book", "2022-12-23", "9"));
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        String[] emptyArray = {"view"};
-        RecurringExpenseList recurringExpenseList = new RecurringExpenseList();
-        expenseList.viewExpense(emptyArray, recurringExpenseList);
-        String expectedOutput  = "Here is the list of your expenses:" + System.lineSeparator()
-                + "     Others     | 2021-12-23 | Cheese burger | $15.50" + System.lineSeparator()
-                + "     Others     | 2022-12-23 | book | $9.00" + System.lineSeparator();
-        assertEquals(expectedOutput, outContent.toString());
-    }
-
-    @Test
-    public void viewExpense_zeroExpenses_success() throws MintException {
-        ExpenseList expenseList = new ExpenseList();
-        RecurringExpenseList recurringExpenseList = new RecurringExpenseList();
-
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
-        String[] emptyArray = {"view"};
-        expenseList.viewExpense(emptyArray, recurringExpenseList);
-        String expectedOutput  = "Here is the list of your expenses:" + System.lineSeparator();
-        assertEquals(expectedOutput, outContent.toString());
-    }
-
-     */
 }
